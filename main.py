29cce7d86bed9f0a695cf8e2bd84f70dec9be6f7
Maruschin Eugene <maruschin@gmail.com>
2017-10-18 16:54:17 +1000

    Добавил обработку параметров вызова через argparse сделал валидаторы.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import argparse
 import urllib.request
 import json
+import re
+from datetime import datetime
 
 
 URL = 'https://api.github.com'
@@ -30,18 +33,40 @@ for b in buzz:
 
 
 
+def main(url, repository, startdate, enddate):
+    print(url)
+    print(repository)
+    print(startdate)
+    print(enddate)
+
+def valid_url(url):
+    if re.match('https://github.com/[a-z0-9-]+/[a-z0-9-]+/?$', url):
+        return url
+    else:
+        msg = "Not a valid github URL: '{0}'.".format(url)
+        raise argparse.ArgumentTypeError(msg)
+
+def valid_date(date):
+    try:
+        return datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        msg = "Not a valid date: '{0}'.".format(date)
+        raise argparse.ArgumentTypeError(msg)
+
 
 if __name__ == "__main__":
-    import argparse
     parser = argparse.ArgumentParser(description='Анализ репозитория github.')
     # URL публичного репозитория на github.com
-    parser.add_argument('URL', metavar='U', type=str, help='URL публичного репозитория на github.com')
+    parser.add_argument('url', type=valid_url,
+        help='The URL of the public repository on github.com')
     # Дата начала анализа. Если пустая, то неограничено.
-    parser.add_argument('--sd', metavar='S', type=str, help='Дата начала анализа. Если пустая, то неограничено')
+    parser.add_argument('-s', '--startdate', type=valid_date,
+        help='Дата начала анализа в формате YYYY-MM-DD')
     # Дата окончания анализа. Если пустая, то неограничено.
-    parser.add_argument('--ed', metavar='E', type=str, help='Дата окончания анализа. Если пустая, то неограничено')
+    parser.add_argument('-e', '--enddate', type=valid_date,
+        help='Дата окончания анализа в формате YYYY-MM-DD')
     # Ветка репозитория. По умолчанию - master.
-    parser.add_argument('--repo', metavar='R', type=str, help='Ветка репозитория. По умолчанию - master')
+    parser.add_argument('-r', '--repository', type=str, default = 'master',
+        help='Ветка репозитория. По умолчанию - master')
 
-    args = parser.parse_args()
-    print(args)
+    main(**(vars(parser.parse_args())))
